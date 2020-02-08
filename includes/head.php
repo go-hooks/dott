d7e631f667ecfc4d410f77fe4aed64bbc3439705
Alex Valdés <alejandro.valdes@gohooks.com>
2020-02-08 11:17:45 -0600

    Initial commit.

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>DOTT Jewelry - Joyería Contemporánea</title>
+        <meta name="description" content="joyería, piercings, aretes, anillos, rainbow, jewelry, mayoreo">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="source/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
+        <link rel="stylesheet" href="css/style.css" id="theme-stylesheet">
+    </head>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+  <?php include 'includes/head.php';?>
+
+  <body>
+    <div class="container-fluid">
+      <div class="row min-vh-100">
+        <div class="col-xl-5 col-lg-6 col-md-8 p-5 p-lg-4 mx-auto d-flex align-items-center">
+          <div>
+            <p> <img src="img/logo.png" class="center"></p>
+            <p class="lead">Estamos trabajando en un sitio especial para ti! Mientras tanto, te invitamos a que nos sigas en nuestras redes sociales!</p>
+            <p class="social">
+              <a href="https://www.facebook.com/DOTT.TRC/" target="_blank" class="external facebook">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://www.instagram.com/dott.trc" target="_blank" class="external instagram">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </p>
+            <p class="copyright">2020 DOTT Jewelry - Todos los derechos reservados</p>
+          </div>
+        </div>
+        <div class="col-xl-6 col-lg-5 col-md-4 pic" style="background-image: url('img/intropic.jfif');"></div>
+      </div>
+    </div>
+    <!-- JS files-->
+    <script src="source/jquery/jquery.min.js"></script>
+    <script src="source/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/front.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  </body>
+
+</html>
